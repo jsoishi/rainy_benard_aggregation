3e09cef88f1dea87cae798eaa429b75eb0979897
Benjamin Brown <bpbrown@gmail.com>
2023-01-08 21:57:43 -0800

    Solver that loops over a range of tau and k, using some clever tricks, to solve a library of atmospheres.

--- python/nlbvp/unsaturated_atm_library.py
+++ python/nlbvp/unsaturated_atm_library.py
@@ -18,14 +18,13 @@ Options:
     --gamma=<gamma>      Gamma parameter [default: 0.3]
     --beta=<beta>        Beta parameter  [default: 1.2]
 
-    --tau=<tau>          Tau parameter [default: 5e-5]
-    --k=<k>              Tanh width of phase change [default: 1e3]
-
     --nz=<nz>            Vertical (z) grid resolution [default: 256]
 """
 import logging
 logger = logging.getLogger(__name__)
 for system in ['h5py._conv', 'matplotlib', 'PIL']:
+     logging.getLogger(system).setLevel(logging.WARNING)
+for system in ['subsystems']:
      logging.getLogger(system).setLevel(logging.WARNING)
 import matplotlib.pyplot as plt
 
@@ -38,9 +37,6 @@ import os
 
 from docopt import docopt
 args = docopt(__doc__)
-
-tau_in = float(args['--tau'])
-k_in = float(args['--k'])
 
 q_surface = float(args['--q0'])
 nz = int(args['--nz'])
@@ -56,15 +52,15 @@ Prandtlm = 1
 P = 1                                 #  diffusion on buoyancy
 S = (Prandtlm/Prandtl)**(-1/2)        #  diffusion on moisture
 
+start_tau = 1e-3
+stop_tau = 5e-5
+taus = np.geomspace(start_tau, stop_tau, num=10)
+ks = np.logspace(2, 3, num=5)
+
 data_dir = 'unsaturated_atm_alpha{:}_gamma{:}_q{:}'.format(args['--alpha'], args['--gamma'], args['--q0'])
-case_dir = 'tau_{:}_k{:}_nz{:d}'.format(args['--tau'], args['--k'], nz)
 
 import dedalus.tools.logging as dedalus_logging
 dedalus_logging.add_file_handler(data_dir+'/logs/dedalus_log', 'DEBUG')
-
-if not os.path.exists('{:s}/'.format(data_dir+'/'+case_dir)):
-    os.mkdir('{:s}/'.format(data_dir+'/'+case_dir))
-
 
 tol = 1e-3
 IC = 'linear' #'LBVP' # 'LBVP' -> compute LBVP, 'linear' (or else) -> use linear ICs
@@ -94,11 +90,6 @@ lift = lambda A, n: de.Lift(A, zb2, n)
 
 ex, ey, ez = coords.unit_vector_fields(dist)
 
-k = dist.Field(name='k')
-k['g'] = k_in
-
-H = lambda A: 0.5*(1+np.tanh(k*A))
-
 z_grid = dist.Field(name='z_grid', bases=zb)
 z_grid['g'] = z
 
@@ -109,7 +100,10 @@ qs = np.exp(α*temp)
 rh = q*np.exp(-α*temp)
 
 tau = dist.Field(name='tau')
+k = dist.Field(name='k')
+H = lambda A: 0.5*(1+np.tanh(k*A))
 
+logger.info('solving LBVP for initial guess; tau, k not needed.')
 dt = lambda A: 0*A
 # Stable linear solution as an intial guess
 problem = de.LBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
@@ -122,10 +116,6 @@ problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
 solver = problem.build_solver()
 solver.solve()
 
-print('b: {:.2g} -- {:.2g}'.format(b(z=0).evaluate()['g'][0,0,0], b(z=Lz).evaluate()['g'][0,0,0]))
-print('q: {:.2g} -- {:.2g}'.format(q(z=0).evaluate()['g'][0,0,0], q(z=Lz).evaluate()['g'][0,0,0]))
-
-
 dt = lambda A: 0*A
 
 # Stable nonlinear solution
@@ -137,25 +127,71 @@ problem.add_equation('b(z=Lz) = β + ΔT') # technically β*Lz
 problem.add_equation('q(z=0) = q_surface*qs(z=0)')
 problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
 
-for system in ['subsystems']:
-     logging.getLogger(system).setLevel(logging.WARNING)
+# for first tau loop, use LBVP or linear solution as first guess
+need_guess = False
 
-tau['g'] = tau_in
-k['g'] = k_in
-solver = problem.build_solver()
-pert_norm = np.inf
-while pert_norm > tol:
-    solver.newton_iteration()
-    pert_norm = sum(pert.allreduce_data_norm('c', 2) for pert in solver.perturbations)
-    logger.info("tau = {:.1g}, k = {:.0g}, L2 err = {:.1g}".format(tau['g'][0,0,0], k['g'][0,0,0], pert_norm))
+# on later tau loops, start with the highest k solution as the first guess, stored in sol
+sol = {}
 
-solution = solver.evaluator.add_file_handler(data_dir+'/'+case_dir+'/'+'drizzle_sol', mode='overwrite')
-solution.add_task(b)
-solution.add_task(q)
-solution.add_task(b + γ*q, name='m')
-solution.add_task(temp, name='T')
-solution.add_task(rh, name='rh')
-# work around for file handlers:
-solver.evaluator.evaluate_handlers([solution], timestep=0, wall_time=0, sim_time=0, iteration=0, world_time=0)
-#solution.process()
-logger.info("wrote solution to {:}/{:}".format(data_dir, case_dir))
+for i, tau_i in enumerate(taus):
+    if i == 0:
+        # on the first sweep, start at low tau and increase tau
+        k_set = ks
+    else:
+        # reverse order on k solves after the first sweep
+        k_set = ks[::-1]
+
+    for j, k_j in enumerate(k_set):
+        case_dir = 'tau_{:.2g}_k{:.2g}_nz{:d}'.format(tau_i, k_j, nz)
+
+        # confused why we need this mkdir call, given filehandler, but let's go with it
+        if not os.path.exists('{:s}/'.format(data_dir+'/'+case_dir)):
+            os.mkdir('{:s}/'.format(data_dir+'/'+case_dir))
+
+        if need_guess:
+            logger.info('solving tau={:}, k={:}, starting from guess'.format(tau_i, k_j))
+            b.change_scales(1)
+            q.change_scales(1)
+            sol['b'].change_scales(1)
+            sol['q'].change_scales(1)
+            b['g'] = sol['b']['g']
+            q['g'] = sol['q']['g']
+            need_guess = False
+        else:
+            logger.info('solving tau={:}, k={:}, continuing from previous solution'.format(tau_i, k_j))
+
+        tau['g'] = tau_i
+        k['g'] = k_j
+
+        solver = problem.build_solver()
+        pert_norm = np.inf
+        while pert_norm > tol:
+            solver.newton_iteration()
+            pert_norm = sum(pert.allreduce_data_norm('c', 2) for pert in solver.perturbations)
+            logger.info("tau = {:.1g}, k = {:.0g}, L2 err = {:.1g}".format(tau['g'][0,0,0], k['g'][0,0,0], pert_norm))
+
+        solution = solver.evaluator.add_file_handler(data_dir+'/'+case_dir+'/'+'drizzle_sol', mode='overwrite')
+        solution.add_task(b)
+        solution.add_task(q)
+        solution.add_task(b + γ*q, name='m')
+        solution.add_task(temp, name='T')
+        solution.add_task(rh, name='rh')
+        solution.add_task(tau, name='tau')
+        solution.add_task(k, name='k')
+        # work around for file handlers:
+        solver.evaluator.evaluate_handlers([solution], timestep=0, wall_time=0, sim_time=0, iteration=0, world_time=0)
+        #solution.process()
+        logger.info("wrote solution to {:}/{:}".format(data_dir, case_dir))
+        if j == 0:
+            need_guess = False
+            if i != 0:
+                logger.info('saving first solution as first guess for next loop')
+                sol['b'] = b.copy()
+                sol['q'] = q.copy()
+
+    if i == 0:
+        logger.info('saving last solution as first guess for next loop')
+        sol['b'] = b.copy()
+        sol['q'] = q.copy()
+
+    need_guess = True
