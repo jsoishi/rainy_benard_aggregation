38d1a2d2e4628db1d391b418d417b5c3c2a70666
Benjamin Brown <bpbrown@gmail.com>
2023-06-25 20:55:00 -0700

    Cleans up logic structure between using analytic solution vs LBVP as initial guess.  Adds L2 error metrics comparing to the analytic soln.

--- python/nlbvp/unsaturated_atm.py
+++ python/nlbvp/unsaturated_atm.py
@@ -18,6 +18,8 @@ Options:
     --gamma=<gamma>      Gamma parameter [default: 0.3]
     --beta=<beta>        Beta parameter  [default: 1.2]
 
+    --use_analytic       Use an analytic solution as the beginning guess
+
     --tau=<tau>          Tau parameter [default: 5e-5]
     --k=<k>              Tanh width of phase change [default: 1e3]
 
@@ -107,18 +109,6 @@ rh = q*np.exp(-α*temp)
 
 tau = dist.Field(name='tau')
 
-dt = lambda A: 0*A
-# Stable linear solution as an intial guess
-problem = de.LBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
-problem.add_equation('dt(b) - P*lap(b) + lift(τb1, -1) + lift(τb2, -2) = 0')
-problem.add_equation('dt(q) - S*lap(q) + lift(τq1, -1) + lift(τq2, -2) = 0')
-problem.add_equation('b(z=0) = 0')
-problem.add_equation('b(z=Lz) = β + ΔT') # technically β*Lz
-problem.add_equation('q(z=0) = q_surface')
-problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
-solver = problem.build_solver()
-solver.solve()
-
 α_f = dist.Field()
 α_f['g'] = α
 β_f = dist.Field()
@@ -127,6 +117,108 @@ solver.solve()
 γ_f['g'] = γ
 
 dt = lambda A: 0*A
+
+from scipy.special import lambertw as W
+def compute_analytic(z_in, zc, Tc):
+    q = dist.Field(bases=zb)
+    T = dist.Field(bases=zb)
+    q.change_scales(dealias)
+    T.change_scales(dealias)
+
+    z = dist.Field(bases=zb)
+    z.change_scales(dealias)
+    z['g'] = z_in
+
+    b1 = 0
+    b2 = β + ΔT
+    q1 = q_surface
+    q2 = np.exp(α*ΔT)
+
+    bc = Tc + β*zc
+    qc = np.exp(α*Tc)
+
+    P = bc + γ*qc
+    Q = ((b2-bc) + γ*(q2-qc))
+
+    C = P + Q*(z['g']-zc)/(1-zc) - β*z['g']
+
+    mask = (z['g']>=zc)
+    T['g'][mask] = C[mask] - W(α*γ*np.exp(α*C[mask])).real/α
+    T['g'][~mask] = Tc*z['g'][~mask]/zc
+
+    b = (T + β*z).evaluate()
+    q['g'][mask] = np.exp(α*T['g'][mask])
+    q['g'][~mask] = q1 + (qc-q1)*z['g'][~mask]/zc
+    m = (b + γ*q).evaluate()
+    rh = (q*np.exp(-α*T)).evaluate()
+    return {'b':b, 'q':q, 'm':m, 'T':T, 'rh':rh, 'z':z, 'γ':γ}
+
+
+#0.4832893544084419	-0.4588071140209613
+zc_analytic = 0.4832893544084419
+Tc_analytic = -0.4588071140209613
+
+analytic = compute_analytic(zb.local_grid(dealias), zc_analytic, Tc_analytic)
+
+def plot_solution(z, solution, title=None, mask=None, linestyle=None, ax=None):
+    b = solution['b']['g']
+    q = solution['q']['g']
+    m = solution['m']['g']
+    T = solution['T']['g']
+    rh = solution['rh']['g']
+    γ = solution['γ']
+
+    if mask is None:
+        mask = np.ones_like(z, dtype=bool)
+    if ax is None:
+        fig, ax = plt.subplots(ncols=2, sharey=True)
+        markup = True
+        return_fig = True
+    else:
+        for axi in ax:
+            axi.set_prop_cycle(None)
+        markup = False
+        return_fig = False
+
+    ax[0].plot(b[mask],z[mask], label='$b$', linestyle=linestyle)
+    ax[0].plot(γ*q[mask],z[mask], label='$\gamma q$', linestyle=linestyle)
+    ax[0].plot(m[mask],z[mask], label='$b+\gamma q$', linestyle=linestyle)
+
+    ax[1].plot(T[mask],z[mask], label='$T$', linestyle=linestyle)
+    ax[1].plot(q[mask],z[mask], label='$q$', linestyle=linestyle)
+    ax[1].plot(rh[mask],z[mask], label='$r_h$', linestyle=linestyle)
+    ax[1].axvline(x=1, linestyle='dotted')
+    if markup:
+        ax[1].legend()
+        ax[0].legend()
+        ax[0].set_ylabel('z')
+        if title:
+            ax[0].set_title(title)
+    if return_fig:
+        return fig, ax
+    else:
+        return ax
+
+fig, ax = plot_solution(zb.local_grid(dealias), analytic)
+fig.savefig(data_dir+'/'+case_dir+'/analytic_solution.png', dpi=300)
+
+if args['--use_analytic']:
+    b.change_scales(dealias)
+    q.change_scales(dealias)
+    b['g'] = analytic['b']['g']
+    q['g'] = analytic['q']['g']
+else:
+    # Stable linear solution as an intial guess
+    problem = de.LBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
+    problem.add_equation('dt(b) - P*lap(b) + lift(τb1, -1) + lift(τb2, -2) = 0')
+    problem.add_equation('dt(q) - S*lap(q) + lift(τq1, -1) + lift(τq2, -2) = 0')
+    problem.add_equation('b(z=0) = 0')
+    problem.add_equation('b(z=Lz) = β + ΔT') # technically β*Lz
+    problem.add_equation('q(z=0) = q_surface')
+    problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
+    solver = problem.build_solver()
+    solver.solve()
+
 
 # Stable nonlinear solution
 problem = de.NLBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
@@ -171,3 +263,17 @@ logger.info('T {:.3g} to {:.3g}'.format(temp(z=0).evaluate()['g'][0,0,0], temp(z
 logger.info('rh {:.3g} to {:.3g}'.format(rh(z=0).evaluate()['g'][0,0,0], rh(z=Lz).evaluate()['g'][0,0,0]))
 if np.isclose(q_surface, 1):
     logger.info('L2(rh - 1) = {:.3g} '.format(np.sqrt(integ((rh-1)**2)).evaluate()['g'][0,0,0]))
+
+def compute_L2_err(sol, analytic):
+    return (integ(np.abs(sol-analytic))/integ(analytic)).evaluate()['g'][0,0,0]
+L2_q = compute_L2_err(q, analytic['q'])
+L2_b = compute_L2_err(b, analytic['b'])
+L2_rh = compute_L2_err(rh, analytic['rh'])
+L2_results = "L2:  q = {:.2g}, b={:.2g}, rh={:.2g}".format(L2_q, L2_b, L2_rh)
+logger.info(L2_results)
+
+m = (b + γ*q).evaluate()
+sol = {'b':b, 'q':q, 'm':m, 'T':temp.evaluate(), 'rh':rh.evaluate(), 'γ':γ}
+fig, ax = plot_solution(zb.local_grid(dealias), sol, title=L2_results)
+plot_solution(zb.local_grid(dealias), analytic, ax=ax, linestyle='dashed')
+fig.savefig(data_dir+'/'+case_dir+'/comparison_to_analytic_solution.png', dpi=300)
