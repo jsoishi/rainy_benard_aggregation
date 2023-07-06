b2d3aa9dcecd9f8d4dd865436e4230984fe4fbc7
Benjamin Brown <bpbrown@gmail.com>
2023-07-06 12:02:27 -0700

    Substantial overhaul of unsaturated_atm_library to include analytic guesses and best practices.

--- python/nlbvp/unsaturated_atm_library.py
+++ python/nlbvp/unsaturated_atm_library.py
@@ -18,7 +18,15 @@ Options:
     --gamma=<gamma>      Gamma parameter [default: 0.3]
     --beta=<beta>        Beta parameter  [default: 1.2]
 
-    --tolerance=<t>      Tolerance for convergence [default: 1e-5]
+    --use_analytic       Use an analytic solution as the beginning guess
+
+    --Legendre
+
+    --erf                Use erf, not tanh
+
+    --tolerance=<t>      Tolerance for convergence [default: 1e-6]
+    --niter=<n>          Max iterations before stopping [default: 15]
+    --damping=<d>        Damping rate for newton iterations [default: 0.9]
 
     --nz=<nz>            Vertical (z) grid resolution [default: 256]
 """
@@ -26,15 +34,13 @@ import logging
 logger = logging.getLogger(__name__)
 for system in ['h5py._conv', 'matplotlib', 'PIL']:
      logging.getLogger(system).setLevel(logging.WARNING)
-for system in ['subsystems']:
-     logging.getLogger(system).setLevel(logging.WARNING)
 import matplotlib.pyplot as plt
 
 import numpy as np
 from dedalus import public as de
 
 from fractions import Fraction
-
+import time
 import os
 
 from docopt import docopt
@@ -49,17 +55,20 @@ nz = int(args['--nz'])
 
 ΔT = -1
 
+start_tau = 1e-3
+stop_tau = 1e-4 #5e-5
+taus = np.geomspace(start_tau, stop_tau, num=5) #10)
+ks = np.logspace(3, 5, num=11)
+
 Prandtl = 1
 Prandtlm = 1
 P = 1                                 #  diffusion on buoyancy
 S = (Prandtlm/Prandtl)**(-1/2)        #  diffusion on moisture
 
-start_tau = 1e-3
-stop_tau = 5e-5
-taus = np.geomspace(start_tau, stop_tau, num=10)
-ks = np.logspace(2, 3, num=5)
+data_dir = 'unsaturated_atm_alpha{:}_beta{:}_gamma{:}_q{:}'.format(args['--alpha'], args['--beta'], args['--gamma'], args['--q0'])
 
-data_dir = 'unsaturated_atm_alpha{:}_gamma{:}_q{:}'.format(args['--alpha'], args['--gamma'], args['--q0'])
+if not os.path.exists('{:s}/'.format(data_dir)):
+    os.mkdir('{:s}/'.format(data_dir))
 
 import dedalus.tools.logging as dedalus_logging
 dedalus_logging.add_file_handler(data_dir+'/logs/dedalus_log', 'DEBUG')
@@ -72,7 +81,10 @@ Lz = 1
 coords = de.CartesianCoordinates('x', 'y', 'z')
 dist = de.Distributor(coords, dtype=np.float64)
 dealias = 2
-zb = de.ChebyshevT(coords.coords[2], size=nz, bounds=(0, Lz), dealias=dealias)
+if args['--Legendre']:
+    zb = de.Legendre(coords.coords[2], size=nz, bounds=(0, Lz), dealias=dealias)
+else:
+    zb = de.ChebyshevT(coords.coords[2], size=nz, bounds=(0, Lz), dealias=dealias)
 z = zb.local_grid(1)
 
 b = dist.Field(name='b', bases=zb)
@@ -83,12 +95,19 @@ q = dist.Field(name='q', bases=zb)
 τq1 = dist.Field(name='τq1')
 τq2 = dist.Field(name='τq2')
 
-zb1 = zb.clone_with(a=zb.a+1, b=zb.b+1)
-zb2 = zb.clone_with(a=zb.a+2, b=zb.b+2)
-lift1 = lambda A, n: de.Lift(A, zb1, n)
-lift = lambda A, n: de.Lift(A, zb2, n)
+lift_basis = zb.derivative_basis(2)
+lift = lambda A, n: de.Lift(A, lift_basis, n)
 
 ex, ey, ez = coords.unit_vector_fields(dist)
+
+k = dist.Field(name='k')
+
+from scipy.special import erf
+if args['--erf']:
+    # go for maximal supression, rather than equivalent width
+    H = lambda A: 0.5*(1+erf(k*A))
+else:
+    H = lambda A: 0.5*(1+np.tanh(k*A))
 
 z_grid = dist.Field(name='z_grid', bases=zb)
 z_grid['g'] = z
@@ -100,8 +119,6 @@ qs = np.exp(α*temp)
 rh = q*np.exp(-α*temp)
 
 tau = dist.Field(name='tau')
-k = dist.Field(name='k')
-H = lambda A: 0.5*(1+np.tanh(k*A))
 
 α_f = dist.Field()
 α_f['g'] = α
@@ -110,30 +127,129 @@ H = lambda A: 0.5*(1+np.tanh(k*A))
 γ_f = dist.Field()
 γ_f['g'] = γ
 
-logger.info('solving LBVP for initial guess; tau, k not needed.')
 dt = lambda A: 0*A
-# Stable linear solution as an intial guess
-problem = de.LBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
-problem.add_equation('dt(b) - P*lap(b) + lift(τb1, -1) + lift(τb2, -2) = 0')
-problem.add_equation('dt(q) - S*lap(q) + lift(τq1, -1) + lift(τq2, -2) = 0')
-problem.add_equation('b(z=0) = 0')
-problem.add_equation('b(z=Lz) = β + ΔT') # technically β*Lz
-problem.add_equation('q(z=0) = q_surface')
-problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
-solver = problem.build_solver()
-solver.solve()
 
-dt = lambda A: 0*A
+from scipy.special import lambertw as W
+def compute_analytic(z_in, zc, Tc):
+    q = dist.Field(bases=zb)
+    T = dist.Field(bases=zb)
+    q.change_scales(dealias)
+    T.change_scales(dealias)
+
+    z = dist.Field(bases=zb)
+    z.change_scales(dealias)
+    z['g'] = z_in
+
+    b1 = 0
+    b2 = β + ΔT
+    q1 = q_surface
+    q2 = np.exp(α*ΔT)
+
+    bc = Tc + β*zc
+    qc = np.exp(α*Tc)
+
+    P = bc + γ*qc
+    Q = ((b2-bc) + γ*(q2-qc))
+
+    C = P + Q*(z['g']-zc)/(1-zc) - β*z['g']
+
+    mask = (z['g']>=zc)
+    T['g'][mask] = C[mask] - W(α*γ*np.exp(α*C[mask])).real/α
+    T['g'][~mask] = Tc*z['g'][~mask]/zc
+
+    b = (T + β*z).evaluate()
+    q['g'][mask] = np.exp(α*T['g'][mask])
+    q['g'][~mask] = q1 + (qc-q1)*z['g'][~mask]/zc
+    m = (b + γ*q).evaluate()
+    rh = (q*np.exp(-α*T)).evaluate()
+    return {'b':b, 'q':q, 'm':m, 'T':T, 'rh':rh, 'z':z, 'γ':γ}
+
+
+#0.4832893544084419	-0.4588071140209613
+if γ == 0.3:
+    zc_analytic = 0.4832893544084419
+    Tc_analytic = -0.4588071140209613
+elif γ == 0.19:
+    zc_analytic = 0.4751621541611023
+    Tc_analytic = -0.4588071140209616
+else:
+    raise ValueError("γ = {:} not yet supported".format(γ))
+
+analytic = compute_analytic(zb.local_grid(dealias), zc_analytic, Tc_analytic)
+
+def plot_solution(z, solution, title=None, mask=None, linestyle=None, ax=None):
+    b = solution['b']['g']
+    q = solution['q']['g']
+    m = solution['m']['g']
+    T = solution['T']['g']
+    rh = solution['rh']['g']
+    γ = solution['γ']
+
+    if mask is None:
+        mask = np.ones_like(z, dtype=bool)
+    if ax is None:
+        fig, ax = plt.subplots(ncols=2, sharey=True)
+        markup = True
+        return_fig = True
+    else:
+        for axi in ax:
+            axi.set_prop_cycle(None)
+        markup = False
+        return_fig = False
+
+    ax[0].plot(b[mask],z[mask], label='$b$', linestyle=linestyle)
+    ax[0].plot(γ*q[mask],z[mask], label='$\gamma q$', linestyle=linestyle)
+    ax[0].plot(m[mask],z[mask], label='$b+\gamma q$', linestyle=linestyle)
+
+    ax[1].plot(T[mask],z[mask], label='$T$', linestyle=linestyle)
+    ax[1].plot(q[mask],z[mask], label='$q$', linestyle=linestyle)
+    ax[1].plot(rh[mask],z[mask], label='$r_h$', linestyle=linestyle)
+    ax[1].axvline(x=1, linestyle='dotted')
+    if markup:
+        ax[1].legend()
+        ax[0].legend()
+        ax[0].set_ylabel('z')
+        if title:
+            ax[0].set_title(title)
+    if return_fig:
+        return fig, ax
+    else:
+        return ax
+
+fig, ax = plot_solution(zb.local_grid(dealias), analytic)
+fig.savefig(data_dir+'/analytic_solution.png', dpi=300)
+
+if args['--use_analytic']:
+    b.change_scales(dealias)
+    q.change_scales(dealias)
+    b['g'] = analytic['b']['g']
+    q['g'] = analytic['q']['g']
+else:
+    # Stable linear solution as an intial guess
+    problem = de.LBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
+    problem.add_equation('dt(b) - P*lap(b) + lift(τb1, -1) + lift(τb2, -2) = 0')
+    problem.add_equation('dt(q) - S*lap(q) + lift(τq1, -1) + lift(τq2, -2) = 0')
+    problem.add_equation('b(z=0) = 0')
+    problem.add_equation('b(z=Lz) = β + ΔT') # technically β*Lz
+    problem.add_equation('q(z=0) = q_surface')
+    problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
+    solver = problem.build_solver()
+    solver.solve()
+
 
 # Stable nonlinear solution
 problem = de.NLBVP([b, q, τb1, τb2, τq1, τq2], namespace=locals())
-problem.add_equation('dt(b) - P*lap(b) + lift(τb1, -1) + lift(τb2, -2) = γ*H(q-qs)*(q-qs)/tau')
-problem.add_equation('dt(q) - S*lap(q) + lift(τq1, -1) + lift(τq2, -2) = - H(q-qs)*(q-qs)/tau')
+problem.add_equation('dt(b) - tau*P*lap(b) + lift(τb1, -1) + lift(τb2, -2) = γ*H(q-qs)*(q-qs)')
+problem.add_equation('dt(q) - tau*S*lap(q) + lift(τq1, -1) + lift(τq2, -2) = - H(q-qs)*(q-qs)')
 problem.add_equation('b(z=0) = 0')
 problem.add_equation('b(z=Lz) = β + ΔT') # technically β*Lz
 problem.add_equation('q(z=0) = q_surface*qs(z=0)')
 problem.add_equation('q(z=Lz) = np.exp(α*ΔT)')
 
+for system in ['subsystems']:
+     logging.getLogger(system).setLevel(logging.WARNING)
+
+start_time = time.time()
 # for first tau loop, use LBVP or linear solution as first guess
 need_guess = False
 
@@ -141,15 +257,15 @@ need_guess = False
 sol = {}
 
 for i, tau_i in enumerate(taus):
-    if i == 0:
-        # on the first sweep, start at low tau and increase tau
-        k_set = ks
-    else:
-        # reverse order on k solves after the first sweep
-        k_set = ks[::-1]
+    # reverse order on k solves
+    k_set = ks[::-1]
 
     for j, k_j in enumerate(k_set):
         case_dir = 'tau_{:.2g}_k{:.2g}_nz{:d}'.format(tau_i, k_j, nz)
+        if args['--erf']:
+            case_dir += '_erf'
+        if args['--Legendre']:
+            case_dir += '_Legendre'
 
         # confused why we need this mkdir call, given filehandler, but let's go with it
         if not os.path.exists('{:s}/'.format(data_dir+'/'+case_dir)):
@@ -169,30 +285,43 @@ for i, tau_i in enumerate(taus):
 
         tau['g'] = tau_i
         k['g'] = k_j
-
         solver = problem.build_solver()
         pert_norm = np.inf
-        while pert_norm > tol:
-            solver.newton_iteration()
+        stop_iteration = int(args['--niter'])
+
+        while pert_norm > tol and solver.iteration <= stop_iteration:
+            solver.newton_iteration(damping=float(args['--damping']))
             pert_norm = sum(pert.allreduce_data_norm('c', 2) for pert in solver.perturbations)
             logger.info("tau = {:.1g}, k = {:.0g}, L2 err = {:.1g}".format(tau['g'][0,0,0], k['g'][0,0,0], pert_norm))
+            τb1_max = np.max(np.abs(τb1['g']))
+            τb2_max = np.max(np.abs(τb2['g']))
+            τq1_max = np.max(np.abs(τq1['g']))
+            τq2_max = np.max(np.abs(τq2['g']))
+            logger.debug("τ L2 errors: τb1={:.1g}, τb2={:.1g}, τq1={:.1g}, τq2={:.1g}".format(τb1_max,τb2_max,τq1_max,τq2_max))
 
-        solution = solver.evaluator.add_file_handler(data_dir+'/'+case_dir+'/'+'drizzle_sol', mode='overwrite')
-        solution.add_task(b)
-        solution.add_task(q)
-        solution.add_task(b + γ*q, name='m')
-        solution.add_task(temp, name='T')
-        solution.add_task(rh, name='rh')
-        solution.add_task(tau, name='tau')
-        solution.add_task(k, name='k')
-        solution.add_task(α_f, name='α')
-        solution.add_task(β_f, name='β')
-        solution.add_task(γ_f, name='γ')
+        if solver.iteration > stop_iteration or not np.isfinite(pert_norm):
+            logger.info("solution failed to converge")
+            logger.info("reverting to analytic solution as guess for next iteration")
+            b.change_scales(dealias)
+            q.change_scales(dealias)
+            b['g'] = analytic['b']['g']
+            q['g'] = analytic['q']['g']
+        else:
+            solution = solver.evaluator.add_file_handler(data_dir+'/'+case_dir+'/'+'drizzle_sol', mode='overwrite')
+            solution.add_task(b)
+            solution.add_task(q)
+            solution.add_task(b + γ*q, name='m')
+            solution.add_task(temp, name='T')
+            solution.add_task(rh, name='rh')
+            solution.add_task(tau, name='tau')
+            solution.add_task(k, name='k')
+            solution.add_task(α_f, name='α')
+            solution.add_task(β_f, name='β')
+            solution.add_task(γ_f, name='γ')
+        #    solution.add_metadata(niter, name='number of iterations')
+            solver.evaluate_handlers()
+            logger.info("wrote solution to {:}/{:}".format(data_dir, case_dir))
 
-        # work around for file handlers:
-        solver.evaluator.evaluate_handlers([solution], timestep=0, wall_time=0, sim_time=0, iteration=0, world_time=0)
-        #solution.process()
-        logger.info("wrote solution to {:}/{:}".format(data_dir, case_dir))
         if j == 0:
             need_guess = False
             if i != 0:
@@ -206,3 +335,5 @@ for i, tau_i in enumerate(taus):
         sol['q'] = q.copy()
 
     need_guess = True
+end_time = time.time()
+logger.info("time to solve: {:.3g}s".format(end_time-start_time))
