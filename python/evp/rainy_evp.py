bd16f7ad7d4fbc60dd41f2740762e5a43720dd1e
Benjamin Brown <bpbrown@gmail.com>
2023-08-31 15:03:56 -0600

    Adds use of squeeze() and [-1] indexing to make system less dimensionally dependent (e.g., 2d and 3d coordinate formulations now both feasible)

--- python/evp/rainy_evp.py
+++ python/evp/rainy_evp.py
@@ -38,9 +38,9 @@ class RainyBenardEVP():
         self.nondim = nondim
         self.bc_type = bc_type
         if self.Legendre:
-            self.zb = de.Legendre(self.coords.coords[2], size=self.nz, bounds=(0, self.Lz), dealias=self.dealias)
+            self.zb = de.Legendre(self.coords.coords[-1], size=self.nz, bounds=(0, self.Lz), dealias=self.dealias)
         else:
-            self.zb = de.ChebyshevT(self.coords.coords[2], size=self.nz, bounds=(0, self.Lz), dealias=self.dealias)
+            self.zb = de.ChebyshevT(self.coords.coords[-1], size=self.nz, bounds=(0, self.Lz), dealias=self.dealias)
 
         self.kx = self.dist.Field(name='kx')
         self.kx['g'] = kx_in
@@ -67,7 +67,7 @@ class RainyBenardEVP():
 
         self.case_name = 'analytic_{:s}/alpha{:1.0f}_beta{:}_gamma{:}_q{:1.1f}'.format(atm_name, self.α,self.β,self.γ, self.lower_q0)
 
-        self.case_name += '/tau{:}_k{:.3e}_relaxation_{:}'.format(self.tau['g'][0,0,0].real,self.k,self.relaxation_method)
+        self.case_name += '/tau{:}_k{:.3e}_relaxation_{:}'.format(self.tau['g'].squeeze().real,self.k,self.relaxation_method)
         if self.erf:
             self.case_name += '_erf'
         if self.Legendre:
@@ -99,13 +99,13 @@ class RainyBenardEVP():
         self.q0.change_scales(1)
         z = self.zb.local_grid(1)
         zd = self.zb.local_grid(self.dealias)
-        p0 = ax[0].plot(self.b0['g'][0,0,:].real, z[0,0,:], label=r'$b$')
-        p1 = ax[0].plot(self.γ*self.q0['g'][0,0,:].real, z[0,0,:], label=r'$\gamma q$')
-        p2 = ax[0].plot(self.b0['g'][0,0,:].real+self.γ*self.q0['g'][0,0,:].real, z[0,0,:], label=r'$m = b + \gamma q$')
-        p3 = ax[0].plot(self.γ*qs0['g'][0,0,:].real, z[0,0,:], linestyle='dashed', alpha=0.3, label=r'$\gamma q_s$')
+        p0 = ax[0].plot(self.b0['g'].squeeze().real, z.squeeze(), label=r'$b$')
+        p1 = ax[0].plot(self.γ*self.q0['g'].squeeze().real, z.squeeze(), label=r'$\gamma q$')
+        p2 = ax[0].plot(self.b0['g'].squeeze().real+self.γ*self.q0['g'].squeeze().real, z.squeeze(), label=r'$m = b + \gamma q$')
+        p3 = ax[0].plot(self.γ*qs0['g'].squeeze().real, z.squeeze(), linestyle='dashed', alpha=0.3, label=r'$\gamma q_s$')
         ax2 = ax[0].twiny()
         self.scrN.change_scales(1)
-        p4 = ax2.plot(self.scrN['g'][0,0,:].real, z[0,0,:], color='xkcd:purple grey', label=r'$\mathcal{N}(z)$')
+        p4 = ax2.plot(self.scrN['g'].squeeze().real, z.squeeze(), color='xkcd:purple grey', label=r'$\mathcal{N}(z)$')
         ax2.set_xlabel(r'$\mathcal{N}(z)$')
         ax2.xaxis.label.set_color('xkcd:purple grey')
         ax2.set_xlim(-0.1,1.1)
@@ -115,15 +115,15 @@ class RainyBenardEVP():
         ax[0].set_xlabel(r'$b$, $\gamma q$, $m$')
         ax[0].set_ylabel(r'$z$')
         #ax[1].plot(q0['g'][0,0,:]-qs0['g'][0,0,:], z[0,0,:])
-        ax[1].plot(de.grad(self.b0).evaluate()['g'][-1][0,0,:].real, zd[0,0,:], label=r'$\nabla b$')
-        ax[1].plot(de.grad(self.γ*self.q0).evaluate()['g'][-1][0,0,:].real, zd[0,0,:], label=r'$\gamma \nabla q$')
-        ax[1].plot(de.grad(self.b0+self.γ*self.q0).evaluate()['g'][-1][0,0,:].real, zd[0,0,:], label=r'$\nabla m$')
+        ax[1].plot(de.grad(self.b0).evaluate()['g'][-1].squeeze().real, zd.squeeze(), label=r'$\nabla b$')
+        ax[1].plot(de.grad(self.γ*self.q0).evaluate()['g'][-1].squeeze().real, zd.squeeze(), label=r'$\gamma \nabla q$')
+        ax[1].plot(de.grad(self.b0+self.γ*self.q0).evaluate()['g'][-1].squeeze().real, zd.squeeze(), label=r'$\nabla m$')
         ax[1].set_xlabel(r'$\nabla b$, $\gamma \nabla q$, $\nabla m$')
         ax[1].legend()
         ax[1].axvline(x=0, linestyle='dashed', color='xkcd:dark grey', alpha=0.5)
         fig.tight_layout()
-        tau_val = self.tau["g"][0,0,0].real
-        Ra_val  = self.Rayleigh["g"][0,0,0].real
+        tau_val = self.tau["g"].squeeze().real
+        Ra_val  = self.Rayleigh["g"].squeeze().real
         filebase = self.case_name+f'/nz_{self.nz}_k_{self.k}_tau_{tau_val:0.1e}_Ra_{Ra_val:0.2e}_evp_background'
         if label:
             filebase += f'_{label}'
@@ -138,6 +138,7 @@ class RainyBenardEVP():
         grad = lambda A: de.grad(A) + ex*dx(A) + ey*dy(A)
         div = lambda A:  de.div(A) + dx(ex@A) + dy(ey@A)
         lap = lambda A: de.lap(A) + dx(dx(A)) + dy(dy(A))
+
         trans = lambda A: de.TransposeComponents(A)
 
         z = self.zb.local_grid(1)
@@ -201,10 +202,11 @@ class RainyBenardEVP():
         else:
             H = lambda A: 0.5*(1+np.tanh(self.k*A))
         # solve NLBVP for smoothing background
-        b0_lower = b0(z=0).evaluate()['g'][0,0,0]
-        b0_upper = b0(z=Lz).evaluate()['g'][0,0,0]
-        q0_lower = q0(z=0).evaluate()['g'][0,0,0]
-        q0_upper = q0(z=Lz).evaluate()['g'][0,0,0]
+        b0_lower = b0(z=0).evaluate()['g'].squeeze()[()]
+        b0_upper = b0(z=Lz).evaluate()['g'].squeeze()[()]
+        q0_lower = q0(z=0).evaluate()['g'].squeeze()[()]
+        q0_upper = q0(z=Lz).evaluate()['g'].squeeze()[()]
+
         lap0 = lambda A: de.lap(A)
         logger.info("relaxing atmosphere via {:}".format(self.relaxation_method))
         for system in ['subsystems']:
@@ -233,8 +235,8 @@ class RainyBenardEVP():
             ivp.add_equation('q0(z=0) = q0_lower')
             ivp.add_equation('q0(z=Lz) = q0_upper')
             ivp_solver = ivp.build_solver(de.SBDF2)
-            Δt = tau['g'][0,0,0].real/4
-            end_time = (1/P).evaluate()['g'][0,0,0].real
+            Δt = tau['g'].squeeze().real/4
+            end_time = (1/P).evaluate()['g'].squeeze().real
             while ivp_solver.sim_time < end_time:
                 ivp_solver.step(Δt)
             logger.info(f"evolved atmosphere to t={ivp_solver.sim_time:.2g} using Δt={Δt} and {ivp_solver.iteration} steps")
