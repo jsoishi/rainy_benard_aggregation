1233d542226fc52e6cbfc6bdaf9c21ed1ef57118
Benjamin Brown <bpbrown@gmail.com>
2023-09-05 23:02:50 -0600

    Old commits to driving scripts.

--- python/evp/saturated_critical_curves.sh
+++ python/evp/saturated_critical_curves.sh
@@ -1,11 +1,13 @@
-python3 convective_onset.py --beta=1.175 --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e7 --num_Ra=2 --erf --Legendre --top-stress-free
+export gamma=0.19
 
-python3 convective_onset.py --beta=1.1625 --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.175 --gamma=$gamma --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e7 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.15 --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.1625 --gamma=$gamma --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.1375 --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.15 --gamma=$gamma --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.1125 --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e5 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.1375 --gamma=$gamma --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e3 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.1 --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e5 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.1125 --gamma=$gamma --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e3 --max_Ra=1e5 --num_Ra=2 --erf --Legendre --top-stress-free
+
+python3 convective_onset.py --beta=1.1 --gamma=$gamma --q0=1 --nz=128 --k=1e4 --tau=0.1 --min_Ra=1e3 --max_Ra=1e5 --num_Ra=2 --erf --Legendre --top-stress-free
--- python/evp/unsaturated_critical_curves.sh
+++ python/evp/unsaturated_critical_curves.sh
@@ -1,13 +1,14 @@
-python3 convective_onset.py --beta=1.15 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e7 --num_Ra=2 --erf --Legendre --top-stress-free
+export nz=128
+# python3 convective_onset.py --beta=1.15 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e8 --num_Ra=2 --erf --Legendre --top-stress-free
+#
+# python3 convective_onset.py --beta=1.125 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e8 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.125 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e7 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.1 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e7 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.1 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e5 --max_Ra=1e7 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.075 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.075 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.05 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.05 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.025 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
 
-python3 convective_onset.py --beta=1.025 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
-
-python3 convective_onset.py --beta=1.0 --q0=0.6 --nz=256 --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
+python3 convective_onset.py --beta=1.0 --q0=0.6 --nz=$nz --k=1e4 --tau=0.1 --min_Ra=1e4 --max_Ra=1e6 --num_Ra=2 --erf --Legendre --top-stress-free
