11014b55156e032741db37c3a5074948660411da
Benjamin Brown <bpbrown@gmail.com>
2024-01-25 13:38:23 -0800

    Working logic for plotting 2.5d slices.

--- python/ivp/plot_slices.py
+++ python/ivp/plot_slices.py
@@ -6,7 +6,7 @@ Usage:
 
 Options:
     --output=<dir>     Output directory; defaults to 'frames' subdir within the case dir
-    --tasks=<tasks>    Tasks to plot [default: b,q,b_fluc,q_fluc,rh,rh_fluc,vorticity]
+    --tasks=<tasks>    Tasks to plot [default: b,q,m,rh,b_fluc,q_fluc,m_fluc,rh_fluc,vorticity]
     --3D_default       plots default 3-D fields
 """
 
@@ -44,7 +44,13 @@ def main(filename, start, count, tasks, output):
             savename_func = lambda write: '{:s}_{:06d}.png'.format(file_name, write)
             task = f['tasks'][task]
             x = task.dims[1][0][:]
+            y = task.dims[2][0][:]
             z = task.dims[3][0][:]
+            if x.size == 1:
+                x = y
+                mask = (0, slice(None), slice(None))
+            else:
+                mask = (slice(None), 0, slice(None))
             Lz = np.max(z)-np.min(z)
             Lx = np.max(x)-np.min(x)
             height = 1.6
@@ -53,11 +59,11 @@ def main(filename, start, count, tasks, output):
                 time = t[k]
                 fig, ax = plt.subplots(1, figsize=figsize)
                 ax.set_aspect(1)
-                pcm = ax.pcolormesh(x, z, task[k,:,0,:].T, shading='nearest',cmap=cmap)
+                pcm = ax.pcolormesh(x, z, task[(k,*mask)].T, shading='nearest',cmap=cmap)
                 pmin,pmax = pcm.get_clim()
                 if center_zero:
                     # use a CDF to find the
-                    H, X = np.histogram(task[k,:], bins = 100, density = True )
+                    H, X = np.histogram(task[(k,*mask)], bins = 100, density = True )
                     dX = X[1] - X[0]
                     F = np.cumsum(H)*dX
                     i_min = np.argmin(np.abs(F-0.05))
@@ -72,7 +78,7 @@ def main(filename, start, count, tasks, output):
                     cNorm = matplotlib.colors.TwoSlopeNorm(vmin=pmin, vcenter=0, vmax=pmax)
                 else:
                     cNorm = matplotlib.colors.Normalize(vmin=pmin, vmax=pmax)
-                pcm = ax.pcolormesh(x, z, task[k,:,0,:].T, shading='nearest',cmap=cmap, norm=cNorm)
+                pcm = ax.pcolormesh(x, z, task[(k,*mask)].T, shading='nearest',cmap=cmap, norm=cNorm)
                 ax_cb = fig.add_axes([0.91, 0.4, 0.02, 1-0.4*2])
                 cb = fig.colorbar(pcm, cax=ax_cb)
                 cb.formatter.set_scientific(True)
