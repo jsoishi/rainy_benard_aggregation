03b4261519d4f588f3d84bf417fa78e2046edbfb
Benjamin Brown <bpbrown@gmail.com>
2024-05-21 21:11:52 -0700

    Driver script for convective onset curves in unsaturated atmospheres.

--- python/evp/unsaturated_critical_curves.sh
+++ python/evp/unsaturated_critical_curves.sh
@@ -1,7 +1,7 @@
 date
-python3 run_critical_curve.py --q0=0.6 --gamma=0.19 --nz=32 &
-python3 run_critical_curve.py --q0=0.6 --gamma=0.3 --nz=32 &
-python3 run_critical_curve.py --q0=0.6 --beta=1 --nz=32 &
-python3 run_critical_curve.py --q0=0.6 --beta=1.1 --nz=32 &
+python3 run_critical_curve.py --q0=0.6 --gamma=0.19 --nz=32 --max_Ra=1e6 --max_kx=5 &
+python3 run_critical_curve.py --q0=0.6 --gamma=0.3 --nz=32 --max_Ra=1e6 --max_kx=5 &
+python3 run_critical_curve.py --q0=0.6 --beta=1 --nz=32 --max_Ra=1e6 --max_kx=5 &
+python3 run_critical_curve.py --q0=0.6 --beta=1.1 --nz=32 --max_Ra=1e6 --max_kx=5 &
 wait
 date
