d57ec1aa0daa1c4a10792f6bd465d3aace177b9f
J. S. Oishi <jsoishi@gmail.com>
2024-06-26 10:44:01 -0400

    added __main__ so functions can be imported elsewhere

--- python/evp/convective_onset_stacked.py
+++ python/evp/convective_onset_stacked.py
@@ -131,79 +131,6 @@ def peak_growth_rate(*args):
     # flip sign so minimize finds maximum
     return -1*rate.real
 
-growth_rates = {}
-ra_list = args['--Ra_list']
-if ra_list != 'None':
-    Ras = [float(r) for r in ra_list.split(',')]
-    print("Ra_list = ", ra_list)
-    print("number of Ras = ",len(ra_list))
-else:
-    Ras = np.geomspace(float(args['--min_Ra']),float(args['--max_Ra']),num=int(float(args['--num_Ra'])))
-kxs = np.geomspace(min_kx, max_kx, num=nkx)
-print(Ras)
-for Ra in Ras:
-    σ = []
-    # reset to base target for each Ra loop
-    target = float(args['--target'])
-    kx = kxs[0]
-    lo_res = SplitRainyBenardEVP(nz, Ra, tau, kx, γ, α, β, q0, k, Legendre=Legendre, erf=erf, bc_type=bc_type, nondim=nondim, dealias=dealias,Lz=1)
-    lo_res.plot_background(plot_type=plot_type)
-    hi_res = SplitRainyBenardEVP(int(3*nz/2), Ra, tau, kx, γ, α, β, q0, k, Legendre=Legendre, erf=erf, bc_type=bc_type, nondim=nondim, dealias=dealias,Lz=1)
-    hi_res.plot_background(plot_type=plot_type)
-    for system in ['rainy_evp']:
-         logging.getLogger(system).setLevel(logging.WARNING)
-
-    for kx in kxs:
-        σ_i = compute_growth_rate(kx, Ra, target=target, plot_type=plot_type)
-        σ.append(σ_i)
-        logger.info('Ra = {:.2g}, kx = {:.2g}, σ = {:.2g}'.format(Ra, kx, σ_i))
-        if σ_i.imag > 0:
-            # update target if on growing branch
-            target = σ_i.imag
-    σ = np.array(σ)
-    growth_rates[Ra] = {'σ':σ, 'max σ.real':σ[np.argmax(σ.real)]}
-
-fig, ax = plt.subplots(figsize=[6,6/1.6])
-
-if nondim == 'diffusion':
-    ax2 = ax.twinx()
-    ax.set_ylim(-15, 25)
-    ax2.set_ylim(1e-1, 1e3)
-    ax2.set_yscale('log')
-    ax.set_ylabel(r'$\omega_R$ (solid)')
-    ax2.set_ylabel(r'$\omega_I$ (dashed)')
-elif nondim == 'buoyancy':
-    ax2 = ax
-    ax.set_ylim(-0.5, 0.5)
-    ax.set_ylabel(r'$\omega_R$ (solid), $\omega_I$ (dashed)')
-
-for Ra in growth_rates:
-    σ = growth_rates[Ra]['σ']
-    p = ax.plot(kxs, σ.real, label='Ra = {:.2g}'.format(Ra))
-    ax2.plot(kxs, σ.imag, linestyle='dashed', color=p[0].get_color())
-ax.set_xscale('log')
-
-fig_filename = 'growth_curves_{:}_nz{:d}'.format(nondim, nz)
-if args['--stress-free']:
-    fig_filename += '_SF'
-if args['--top-stress-free']:
-    fig_filename += '_TSF'
-if args['--dense']:
-    fig_filename += '_dense'
-ax.legend()
-ax.axhline(y=0, linestyle='dashed', color='xkcd:grey', alpha=0.5)
-ax.set_title(r'$\gamma$ = {:}, $\beta$ = {:}, $\tau$ = {:}'.format(γ,β,tau))
-ax.set_xlabel('$k_x$')
-ax.set_ylim(-0.1,0.1)
-ax.set_title('{:} timescales'.format(nondim))
-plt.tight_layout()
-fig.savefig(lo_res.case_name+'/'+fig_filename+'.png', dpi=300)
-
-for system in ['rainy_evp']:
-     logging.getLogger(system).setLevel(logging.WARNING)
-
-import scipy.optimize as sciop
-bounds = sciop.Bounds(lb=1, ub=10)
 def find_continous_peak(Ra, kx, plot_fastest_mode=False, plot_type='png'):
 
     result = sciop.minimize(peak_growth_rate, kx, args=(Ra), bounds=bounds, method='Nelder-Mead', tol=1e-5)
@@ -211,100 +138,174 @@ def find_continous_peak(Ra, kx, plot_fastest_mode=False, plot_type='png'):
     σ = compute_growth_rate(result.x[0], Ra, plot_fastest_mode=plot_fastest_mode, plot_type=plot_type)
     return result.x[0], σ
 
-# find Ra bracket
-σ_re = -np.inf
-lower_Ra = None
-upper_Ra = None
-for Ra in growth_rates:
-    σ_re = growth_rates[Ra]['max σ.real']
-    if σ_re < 0:
-        lower_Ra = Ra
+if __name__ == "__main__":
+    growth_rates = {}
+    ra_list = args['--Ra_list']
+    if ra_list != 'None':
+        Ras = [float(r) for r in ra_list.split(',')]
+        print("Ra_list = ", ra_list)
+        print("number of Ras = ",len(ra_list))
     else:
-        upper_Ra = Ra
-        break
-if not lower_Ra or not upper_Ra:
-    raise ValueError("Sampled Rayleigh numbers do not bound instability (lower: {:}, upper: {:})".format(lower_Ra, upper_Ra))
+        Ras = np.geomspace(float(args['--min_Ra']),float(args['--max_Ra']),num=int(float(args['--num_Ra'])))
+    kxs = np.geomspace(min_kx, max_kx, num=nkx)
+    print(Ras)
+    for Ra in Ras:
+        σ = []
+        # reset to base target for each Ra loop
+        target = float(args['--target'])
+        kx = kxs[0]
+        lo_res = SplitRainyBenardEVP(nz, Ra, tau, kx, γ, α, β, q0, k, Legendre=Legendre, erf=erf, bc_type=bc_type, nondim=nondim, dealias=dealias,Lz=1)
+        lo_res.plot_background(plot_type=plot_type)
+        hi_res = SplitRainyBenardEVP(int(3*nz/2), Ra, tau, kx, γ, α, β, q0, k, Legendre=Legendre, erf=erf, bc_type=bc_type, nondim=nondim, dealias=dealias,Lz=1)
+        hi_res.plot_background(plot_type=plot_type)
+        for system in ['rainy_evp']:
+             logging.getLogger(system).setLevel(logging.WARNING)
 
-# find peak growth rates of bracket
-peaks = {}
-for Ra in [lower_Ra, upper_Ra]:
-    σ = growth_rates[Ra]['σ']
-    peak_i = np.argmax(σ.real)
-    kx0 = kxs[peak_i] # initial guess
-    kx, σ = find_continous_peak(Ra, kx0, plot_type=plot_type)
-    peaks[Ra] = {'σ':σ, 'k':kx}
+        for kx in kxs:
+            σ_i = compute_growth_rate(kx, Ra, target=target, plot_type=plot_type)
+            σ.append(σ_i)
+            logger.info('Ra = {:.2g}, kx = {:.2g}, σ = {:.2g}'.format(Ra, kx, σ_i))
+            if σ_i.imag > 0:
+                # update target if on growing branch
+                target = σ_i.imag
+        σ = np.array(σ)
+        growth_rates[Ra] = {'σ':σ, 'max σ.real':σ[np.argmax(σ.real)]}
 
-# conduct a bracketing search with interpolation to find critical Ra
-σ = np.inf
-tol = float(args['--tol_crit_Ra'])
-iter = 0
-max_iter = 15
-while np.abs(σ.real) > tol and iter < max_iter:
-    σs = np.array([peaks[lower_Ra]['σ'], peaks[upper_Ra]['σ']])
-    ks = np.array([peaks[lower_Ra]['k'], peaks[upper_Ra]['k']])
-    ln_Ras = np.log(np.array([lower_Ra, upper_Ra]))
-    # do this in log Ra
-    ln_crit_Ra = np.interp(0, σs.real, ln_Ras)
-    crit_k = np.interp(ln_crit_Ra, ln_Ras, ks)
-    crit_σ = np.interp(ln_crit_Ra, ln_Ras, σs)
-    crit_Ra = np.exp(ln_crit_Ra)
+    fig, ax = plt.subplots(figsize=[6,6/1.6])
 
-    logger.info('Critical point, based on interpolation:')
-    logger.info('Ra = {:.3g}, k = {:}'.format(crit_Ra, crit_k))
+    if nondim == 'diffusion':
+        ax2 = ax.twinx()
+        ax.set_ylim(-15, 25)
+        ax2.set_ylim(1e-1, 1e3)
+        ax2.set_yscale('log')
+        ax.set_ylabel(r'$\omega_R$ (solid)')
+        ax2.set_ylabel(r'$\omega_I$ (dashed)')
+    elif nondim == 'buoyancy':
+        ax2 = ax
+        ax.set_ylim(-0.5, 0.5)
+        ax.set_ylabel(r'$\omega_R$ (solid), $\omega_I$ (dashed)')
 
-    kx, σ = find_continous_peak(crit_Ra, crit_k, plot_fastest_mode=True)
-    logger.info('σ = {:.2g}, {:.2g}i (calculated) at k = {:}'.format(σ.real, σ.imag, kx))
-
-    if σ.real > 0:
-        upper_Ra = crit_Ra
-    else:
-        lower_Ra = crit_Ra
-    if not crit_Ra in peaks:
-        peaks[crit_Ra] = {'σ':σ, 'k':kx}
-    crit_k = kx
-    iter+=1
-
-logger.info("critical Ra found after {:d} iterations".format(iter))
-
-for Ra in peaks:
-    ax.scatter(peaks[Ra]['k'], peaks[Ra]['σ'].real, color='xkcd:grey', marker='x', alpha=0.5)
-
-ax.scatter(crit_k, σ.real, color='xkcd:pink', marker='o', alpha=0.5)
-
-fig_filename = 'growth_curves_peaks_{:}_nz{:d}'.format(nondim, nz)
-if args['--stress-free']:
-    fig_filename += '_SF'
-if args['--top-stress-free']:
-    fig_filename += '_TSF'
-if args['--dense']:
-    fig_filename += '_dense'
-fig_filename = f"{lo_res.case_name}/{fig_filename}.{plot_type}"
-fig.savefig(fig_filename, dpi=300)
-logger.info("peaks plotted in {:}".format(fig_filename))
-
-f_curves = lo_res.case_name+'/critical_curves_nz_{:d}.h5'.format(nz)
-with h5py.File(f_curves,'w') as f:
-    f['α'] = α
-    f['β'] = β
-    f['γ'] = γ
-    f['q0'] = q0
-    f['tau'] = tau
-    f['k_q'] = k
-    f['nz'] = nz
-    # critical value
-    f['crit/Ra'] = crit_Ra
-    f['crit/k'] = crit_k
-    f['crit/σ'] = peaks[crit_Ra]['σ']
-    f['crit/iter'] = iter
-    f['crit/max_iter'] = max_iter
-    f['crit/tol'] = tol
-    for i, Ra in enumerate(peaks):
-        f[f'peaks/{i:d}/Ra'] = Ra
-        f[f'peaks/{i:d}/k'] = peaks[Ra]['k']
-        f[f'peaks/{i:d}/σ'] = peaks[Ra]['σ']
     for Ra in growth_rates:
-        f[f'curves/{Ra:.4e}/Ra'] = Ra
-        f[f'curves/{Ra:.4e}/k'] = kxs
-        f[f'curves/{Ra:.4e}/σ'] = growth_rates[Ra]['σ']
-    f.close()
-logger.info("Critical curves written out to: {:}".format(f_curves))
+        σ = growth_rates[Ra]['σ']
+        p = ax.plot(kxs, σ.real, label='Ra = {:.2g}'.format(Ra))
+        ax2.plot(kxs, σ.imag, linestyle='dashed', color=p[0].get_color())
+    ax.set_xscale('log')
+
+    fig_filename = 'growth_curves_{:}_nz{:d}'.format(nondim, nz)
+    if args['--stress-free']:
+        fig_filename += '_SF'
+    if args['--top-stress-free']:
+        fig_filename += '_TSF'
+    if args['--dense']:
+        fig_filename += '_dense'
+    ax.legend()
+    ax.axhline(y=0, linestyle='dashed', color='xkcd:grey', alpha=0.5)
+    ax.set_title(r'$\gamma$ = {:}, $\beta$ = {:}, $\tau$ = {:}'.format(γ,β,tau))
+    ax.set_xlabel('$k_x$')
+    ax.set_ylim(-0.1,0.1)
+    ax.set_title('{:} timescales'.format(nondim))
+    plt.tight_layout()
+    fig.savefig(lo_res.case_name+'/'+fig_filename+'.png', dpi=300)
+
+    for system in ['rainy_evp']:
+         logging.getLogger(system).setLevel(logging.WARNING)
+
+    import scipy.optimize as sciop
+    bounds = sciop.Bounds(lb=1, ub=10)
+    # find Ra bracket
+    σ_re = -np.inf
+    lower_Ra = None
+    upper_Ra = None
+    for Ra in growth_rates:
+        σ_re = growth_rates[Ra]['max σ.real']
+        if σ_re < 0:
+            lower_Ra = Ra
+        else:
+            upper_Ra = Ra
+            break
+    if not lower_Ra or not upper_Ra:
+        raise ValueError("Sampled Rayleigh numbers do not bound instability (lower: {:}, upper: {:})".format(lower_Ra, upper_Ra))
+
+    # find peak growth rates of bracket
+    peaks = {}
+    for Ra in [lower_Ra, upper_Ra]:
+        σ = growth_rates[Ra]['σ']
+        peak_i = np.argmax(σ.real)
+        kx0 = kxs[peak_i] # initial guess
+        kx, σ = find_continous_peak(Ra, kx0, plot_type=plot_type)
+        peaks[Ra] = {'σ':σ, 'k':kx}
+
+    # conduct a bracketing search with interpolation to find critical Ra
+    σ = np.inf
+    tol = float(args['--tol_crit_Ra'])
+    iter = 0
+    max_iter = 15
+    while np.abs(σ.real) > tol and iter < max_iter:
+        σs = np.array([peaks[lower_Ra]['σ'], peaks[upper_Ra]['σ']])
+        ks = np.array([peaks[lower_Ra]['k'], peaks[upper_Ra]['k']])
+        ln_Ras = np.log(np.array([lower_Ra, upper_Ra]))
+        # do this in log Ra
+        ln_crit_Ra = np.interp(0, σs.real, ln_Ras)
+        crit_k = np.interp(ln_crit_Ra, ln_Ras, ks)
+        crit_σ = np.interp(ln_crit_Ra, ln_Ras, σs)
+        crit_Ra = np.exp(ln_crit_Ra)
+
+        logger.info('Critical point, based on interpolation:')
+        logger.info('Ra = {:.3g}, k = {:}'.format(crit_Ra, crit_k))
+
+        kx, σ = find_continous_peak(crit_Ra, crit_k, plot_fastest_mode=True)
+        logger.info('σ = {:.2g}, {:.2g}i (calculated) at k = {:}'.format(σ.real, σ.imag, kx))
+
+        if σ.real > 0:
+            upper_Ra = crit_Ra
+        else:
+            lower_Ra = crit_Ra
+        if not crit_Ra in peaks:
+            peaks[crit_Ra] = {'σ':σ, 'k':kx}
+        crit_k = kx
+        iter+=1
+
+    logger.info("critical Ra found after {:d} iterations".format(iter))
+
+    for Ra in peaks:
+        ax.scatter(peaks[Ra]['k'], peaks[Ra]['σ'].real, color='xkcd:grey', marker='x', alpha=0.5)
+
+    ax.scatter(crit_k, σ.real, color='xkcd:pink', marker='o', alpha=0.5)
+
+    fig_filename = 'growth_curves_peaks_{:}_nz{:d}'.format(nondim, nz)
+    if args['--stress-free']:
+        fig_filename += '_SF'
+    if args['--top-stress-free']:
+        fig_filename += '_TSF'
+    if args['--dense']:
+        fig_filename += '_dense'
+    fig_filename = f"{lo_res.case_name}/{fig_filename}.{plot_type}"
+    fig.savefig(fig_filename, dpi=300)
+    logger.info("peaks plotted in {:}".format(fig_filename))
+
+    f_curves = lo_res.case_name+'/critical_curves_nz_{:d}.h5'.format(nz)
+    with h5py.File(f_curves,'w') as f:
+        f['α'] = α
+        f['β'] = β
+        f['γ'] = γ
+        f['q0'] = q0
+        f['tau'] = tau
+        f['k_q'] = k
+        f['nz'] = nz
+        # critical value
+        f['crit/Ra'] = crit_Ra
+        f['crit/k'] = crit_k
+        f['crit/σ'] = peaks[crit_Ra]['σ']
+        f['crit/iter'] = iter
+        f['crit/max_iter'] = max_iter
+        f['crit/tol'] = tol
+        for i, Ra in enumerate(peaks):
+            f[f'peaks/{i:d}/Ra'] = Ra
+            f[f'peaks/{i:d}/k'] = peaks[Ra]['k']
+            f[f'peaks/{i:d}/σ'] = peaks[Ra]['σ']
+        for Ra in growth_rates:
+            f[f'curves/{Ra:.4e}/Ra'] = Ra
+            f[f'curves/{Ra:.4e}/k'] = kxs
+            f[f'curves/{Ra:.4e}/σ'] = growth_rates[Ra]['σ']
+        f.close()
+    logger.info("Critical curves written out to: {:}".format(f_curves))
