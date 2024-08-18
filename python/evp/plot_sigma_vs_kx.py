2e4aff678780000fa0551fdf1a8ad077d3e863ef
Jeff Oishi <jso1018@login01.rcchpc>
2024-08-18 16:38:34 -0400

    Better formatting for Rayleigh number
    * also added ability to skip a Ra in the list

--- python/evp/plot_sigma_vs_kx.py
+++ python/evp/plot_sigma_vs_kx.py
@@ -2,8 +2,14 @@ import sys
 from pathlib import Path
 import h5py
 import matplotlib.pyplot as plt
+
+import matplotlib.ticker as mticker
+
+f = mticker.ScalarFormatter(useMathText=True)
+
 plt.style.use('prl')
 
+exclude = None
 crit_index = 2
 filename = Path(sys.argv[-1])
 filedir = filename.parent
@@ -11,15 +17,18 @@ fig, ax = plt.subplots(figsize=[8,8/1.6])
 with h5py.File(filename, 'r') as df:
     Ra_list = {k:float(k) for k in list(df['curves'].keys())}
     for i,key in enumerate(sorted(Ra_list, key=Ra_list.get, reverse=True)):
-        σ = df[f'curves/{key}']['σ'][:]
-        k = df[f'curves/{key}']['k'][:]
-        if i == crit_index:
-            color = 'C0'
+        if i == exclude:
+            print(f"Skipping Ra ={key}")
         else:
-            color = f'{i/5}'
-        ax.semilogx(k, σ.real, label=f'Ra = {Ra_list[key]:,.0f}',color=color)
+            σ = df[f'curves/{key}']['σ'][:]
+            k = df[f'curves/{key}']['k'][:]
+            if i == crit_index:
+                color = 'C0'
+            else:
+                color = f'{i/5}'
+            ax.semilogx(k, σ.real, label=f'$Ra = {f.format_data(Ra_list[key])}$',color=color)
 ax.axhline(0, color='k', linestyle=':',linewidth=1)
-ax.legend()
+ax.legend(fontsize=16)
 ax.set_ylim(-0.1,0.1)
 ax.set_xlim(0.1,10)
 ax.set_xlabel(r"$k_x$")
