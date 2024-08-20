d7844c99d34a0bd1edeeaa7017a5d6aa74687cae
Benjamin Brown <bpbrown@gmail.com>
2024-08-20 12:10:08 -0700

    Shell script documenting choices in spectra plots.

---
+++ python/evp/make_spectra_plots.sh
@@ -0,0 +1,5 @@
+python3 single_spectrum_stacked.py analytic --beta=1.05 --kx=2.58 --top-stress-free --erf --k=1e4 --nz=128 --Ra=2.654e4 --rejection_method=taus --Legendre --dense --use-heaviside
+
+python3 single_spectrum_stacked.py analytic --beta=1.05 --kx=4 --top-stress-free --erf --k=1e4 --nz=128 --Ra=2.65e5 --rejection_method=taus --Legendre --dense --use-heaviside
+
+python3 single_spectrum_stacked.py analytic --beta=1.05 --kx=6.5 --top-stress-free --erf --k=1e4 --nz=128 --Ra=2.65e6 --rejection_method=taus --Legendre --dense --use-heaviside
