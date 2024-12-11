ac0fc9b52b11e8af6094b3f2dd5a97af0b91927e
Benjamin Brown <bpbrown@gmail.com>
2024-12-11 11:10:47 -0700

    Final settings for tau=1e-2 curves

--- python/evp/run_plot_critical_unsaturated_tau1e-2.sh
+++ python/evp/run_plot_critical_unsaturated_tau1e-2.sh
@@ -1,1 +1,7 @@
-python3 plot_critical_Ra.py analytic_unsaturated/*/tau0.01_k1.000e+04_erf_Legendre_heaviside/critical_curves_nz_64.h5 --q0=0.6 $*
+python3 plot_critical_Ra.py \
+     analytic_unsaturated/*/tau0.01_k1.000e+05_erf_Legendre_heaviside/critical_curves_adaptive_nz_128.h5 \
+     --q0=0.6 $*
+     #analytic_unsaturated/*/tau0.001_k1.000e+05_erf_Legendre_heaviside/critical_curves_adaptive_nz_128.h5 \     
+
+     #analytic_unsaturated/*/tau0.01_k1.000e+05_erf_Legendre/critical_curves_adaptive_nz_64.h5 \
+     #analytic_unsaturated/*/tau0.01_k1.000e+05_erf_Legendre_heaviside/critical_curves_adaptive_nz_64.h5 \
--- python/evp/unsaturated_critical_curves_1e-2.sh
+++ python/evp/unsaturated_critical_curves_1e-2.sh
@@ -1,8 +1,8 @@
 date
-python3 run_critical_curve.py --q0=0.6 --gamma=0.19 --nz=64 --tau=1e-2 --max_Ra=1e6 --num_Ra=7 --max_kx=5 --use-heaviside &
-python3 run_critical_curve.py --q0=0.6 --gamma=0.3 --nz=64 --tau=1e-2 --max_Ra=1e6 --num_Ra=7 --max_kx=5 --use-heaviside &
-python3 run_critical_curve.py --q0=0.6 --beta=1 --nz=64 --tau=1e-2 --max_Ra=1e6 --num_Ra=7 --max_kx=5 --use-heaviside &
-python3 run_critical_curve.py --q0=0.6 --beta=1.05 --nz=64 --tau=1e-2 --max_Ra=1e6 --num_Ra=7 --max_kx=5 --use-heaviside &
-python3 run_critical_curve.py --q0=0.6 --beta=1.1 --nz=64 --tau=1e-2 --max_Ra=1e6 --num_Ra=7 --max_kx=5 --use-heaviside &
+python3 run_critical_curve.py --q0=0.6 --gamma=0.19 --nz=128 --tau=1e-2 --k=1e5 --use-heaviside --continuation &
+python3 run_critical_curve.py --q0=0.6 --gamma=0.3 --nz=128 --tau=1e-2 --k=1e5 --use-heaviside --continuation &
+python3 run_critical_curve.py --q0=0.6 --beta=1 --nz=128 --tau=1e-2 --k=1e5 --use-heaviside --continuation &
+python3 run_critical_curve.py --q0=0.6 --beta=1.05 --nz=128 --tau=1e-2 --k=1e5 --use-heaviside --continuation &
+python3 run_critical_curve.py --q0=0.6 --beta=1.1 --nz=128 --tau=1e-2 --k=1e5 --use-heaviside --continuation &
 wait
 date
