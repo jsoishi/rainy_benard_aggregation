d3aa879a19df5687f0a0a3865e049ab6cfb1d717
Benjamin Brown <bpbrown@gmail.com>
2024-12-15 14:25:20 -0700

    Moves single_spectrum_stacked to single_spectrum, cleans up associated driver scripts.

--- python/evp/plot_spectra_and_eigenmodes_unsaturated_beta1.05.sh
+++ python/evp/plot_spectra_and_eigenmodes_unsaturated_beta1.05.sh
@@ -1,8 +1,8 @@
-python3 single_spectrum_stacked.py --top-stress-free --Legendre --erf --use-heaviside --eigs=50 \
-                                   --nz=256 --plot=pdf --beta=1.05 --Ra=2.68e4 --kx=2.57 --norm='b'
+python3 single_spectrum.py --top-stress-free --Legendre --erf --use-heaviside --eigs=50 \
+                           --nz=256 --plot=pdf --beta=1.05 --Ra=2.68e4 --kx=2.57 --norm='b'
 
-python3 single_spectrum_stacked.py --top-stress-free --Legendre --erf --use-heaviside --eigs=50 \
-                                   --nz=256 --plot=pdf --beta=1.05 --Ra=2.68e5 --kx=4
+python3 single_spectrum.py --top-stress-free --Legendre --erf --use-heaviside --eigs=50 \
+                           --nz=256 --plot=pdf --beta=1.05 --Ra=2.68e5 --kx=4
 
-python3 single_spectrum_stacked.py --top-stress-free --Legendre --erf --use-heaviside --eigs=50 \
-                                   --nz=256 --plot=pdf --beta=1.05 --Ra=2.68e6 --kx=6.5
+python3 single_spectrum.py --top-stress-free --Legendre --erf --use-heaviside --eigs=50 \
+                           --nz=256 --plot=pdf --beta=1.05 --Ra=2.68e6 --kx=6.5
